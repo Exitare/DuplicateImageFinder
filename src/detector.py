a235ee809f3b567e4fc81963ddecf01c8ffb4fb8
Exitare <business.exitare@icloud.com>
2020-06-21 16:12:09 -0700

    Code cleanup

--- src/detector.py
+++ src/detector.py
@@ -1,19 +1,9 @@
-import hashlib
-# from scipy.misc import imread, imresize, imshow
-# import matplotlib.pyplot as plt
-# import matplotlib.gridspec as gridspec
-import time
-# import numpy as np
-from Controllers import BaseController
-from flask import Flask, escape, request, jsonify, send_from_directory, render_template
-from flask_restful import Resource, Api
-from waitress import serve
+from flask import Flask, send_from_directory, render_template
+from flask_restful import Api
 import argparse
 import os
 import hashlib
 from pathlib import Path
-import webbrowser
-import shutil
 from random import randrange
 
 app = Flask(__name__, static_folder='static')
@@ -30,13 +20,8 @@ def root():
     return render_template("index.html", duplicates=duplicates)
 
 
-@app.route('/test')
-def test():
-    return send_from_directory("./static", "IMG_1993.jpeg")
-
-
-def file_hash(filepath):
-    with open(filepath, 'rb') as f:
+def file_hash(file_path: str):
+    with open(file_path, 'rb') as f:
         return md5(f.read()).hexdigest()
 
 
@@ -50,10 +35,8 @@ def handle_args():
                                      epilog='Accepts tsv and csv files')
     parser.add_argument('-v', '--verbose', dest='verbose', action='store', required=False,
                         help="Enables the verbose mode. With active verbose mode additional information is shown in the console")
-    parser.add_argument('-f', '--folder', dest='folder', default=False, required=False,
+    parser.add_argument('-f', '--folder', dest='folder', default=False, required=True,
                         help="The folder which should be checked for duplicates")
-    parser.add_argument('-r', '--remove', dest='remove', action='store', required=False,
-                        help="Activates the removal of data for further evaluation of data sets")
     args = parser.parse_args()
 
 
@@ -80,7 +63,7 @@ def find_duplicates():
                 duplicates[file_hash] = [Path(subdir, file)]
             else:
                 print("in")
-                file 
+                file
                 duplicates[file_hash].append(Path(subdir, file))
 
     # Remove images which do not have duplicates
@@ -112,6 +95,10 @@ def symbol_links():
                 extension = os.path.splitext(os.path.basename(image))[1]
                 name = os.path.splitext(os.path.basename(image))[0]
                 os.symlink(image, Path("src", "static", f"{name}_{rand}{extension}"))
+
+
+def clean_folder(path: Path):
+    pass
 
 
 if __name__ == '__main__':
